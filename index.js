67bc04a9903ae8306b43267ad01ab5f2d5bcbaca
Anastasiya Besman <staenrey@gmail.com>
2021-02-18 16:57:04 +1100

    deleted files not needed for stage 3c

--- index.js
+++ index.js
@@ -17,61 +17,23 @@ app.use(expressLayouts)
 app.set("layout", "pages/layouts/basiclayout")
 app.set("view engine", "ejs")
 
+
 app.get("/", (req, res) => {
-    res.render("pages/welcome")
-})
-
-app.get("/users", (req, res) => {
-    res.render("pages/users", {users: data.users}) // alternatively, {data}
-})
-
-app.get("/schedules", (req, res) => {
     res.render("pages/schedules", {schedules: data.schedules, users: data.users, weekDays: utils.weekDays})
 })
 
-//most specific route should go first, see line 39
-app.get("/users/new", (req, res) => { 
-    res.render("pages/adduser")
-})
+//most specific route should go first
 
-
-app.get("/users/:userId", (req, res) => {
-    // res.send(data.users[req.params.userId])
-    res.render("pages/userprofile", {userprofile: data.users[req.params.userId]})
-})
-
-// check if everything is in camelCase
-
-app.get("/users/:userId/schedules", (req, res) => {
-    const userSchedules = [];
-    for (i = 0; i < data.schedules.length; i++) {
-        if (data.schedules[i].user_id === parseInt(req.params.userId, 10)) {
-            userSchedules.push(data.schedules[i])
-        }
-    }
-    // res.send(userSchedules)
-    res.render("pages/userschedule", {schedules: userSchedules, weekDays: utils.weekDays, userprofile: data.users[req.params.userId]})
-})
-
-app.get("/schedules/new", (req, res) => {
+app.get("/new", (req, res) => {
     res.render("pages/addschedule", {users: data.users})
 })
 
-app.post("/schedules", (req, res) => {
+app.post("/new", (req, res) => {
     const newSchedule = req.body
     newSchedule.user_id = parseInt(newSchedule.user_id, 10)
     newSchedule.day = parseInt(newSchedule.day, 10)
     data.schedules.push(newSchedule)
-    res.redirect("/schedules")
-})
-
-app.post("/users", (req, res) => {
-    
-    const password = req.body['password']
-    const hash = crypto.createHash("sha256").update(password).digest("base64")
-    req.body.password = hash
-    data.users.push(req.body)
-    res.redirect("/users")
+    res.redirect("/")
 })
 
 app.listen(port, () => {
